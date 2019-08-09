f44dec7993820f4faa7f6ecd803f3e1d6ea63fc3
Jeffrey Brown <brownjs03@gmail.com>
2019-08-09 14:21:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,23 +8,23 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :names {},
-      :rent_in_dollars {}
+      :names => {},
+      :rent_in_dollars => {}
     }
   }
 end
 
 def monopoly_with_third_tier
 	monopoly = {
-    :railroads {
-      :pieces 4,
-      :names {
+    :railroads => {
+      :pieces => 4,
+      :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
         :shortline => {}
       },
-      :rent_in_dollars {
+      :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
@@ -36,9 +36,9 @@ end
 
 def monopoly_with_fourth_tier
   monopoly = {
-    :railroads {
-      :pieces 4,
-      :names {
+    :railroads => {
+      :pieces => 4,
+      :names => {
         :reading_railroad => {
           "mortgage_value" => "$100"
           },
